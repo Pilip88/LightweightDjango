1d411b40ee982d79105bc165d714f220d78baabb
Pilip88 <pilipovicivan@yahoo.com>
2015-12-20 13:03:50 +0100

    Initial commit.

---
+++ hello_django/hello.py
@@ -0,0 +1,46 @@
+# view
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Hello World')
+
+# urls
+from django.conf.urls import url
+
+urlpatterns = (
+        url(r'^$', index),
+    )
+
+# settings
+import os
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+        DEBUG=True,
+        SECRET_KEY=SECRET_KEY,
+        ALLOWED_HOSTS=ALLOWED_HOSTS,
+        ROOT_URLCONF=__name__,
+        MIDDLEWARE_CLASSES=(
+                'django.middleware.common.CommonMiddleware',
+                'django.middleware.csrf.CsrfViewMiddleware',
+                'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            ),
+    )
+
+# wsgi
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
+# manage
+import sys
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
